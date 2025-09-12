27298778e67d158714910cd092d36cc3530ef71d
Xander <gouws.xander@gmail.com>
2025-09-12 11:31:36 -0300

    Initial Commit

---
+++ Reef/ReefApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReefApp.swift
+//  Reef
+//
+//  Created by Xander Gouws on 12-09-2025.
+//
+
+import SwiftUI
+
+@main
+struct ReefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReefTests/ReefTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReefTests.swift
+//  ReefTests
+//
+//  Created by Xander Gouws on 12-09-2025.
+//
+
+import Testing
+@testable import Reef
+
+struct ReefTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
