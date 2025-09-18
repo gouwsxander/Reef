62dff6934cc41cef3812b884da851465074fb1a9
Xander <gouws.xander@gmail.com>
2025-09-18 13:59:52 -0300

    Refactors AXUIElement to ApplicationServices

--- Reef/logic/ApplicationServices.swift
+++ Reef/logic/ApplicationServices.swift
@@ -1,5 +1,5 @@
 //
-//  AXUIElement.swift
+//  ApplicationServices.swift
 //  Reef
 //
 //  Created by Xander Gouws on 16-09-2025.
@@ -8,17 +8,20 @@
 import Foundation
 import Cocoa
 
+/*
+ Adds Swift wrappers around the accessibility object class.
+ */
 extension AXUIElement {
-    func getValue<T>(_ attribute: NSAccessibility.Attribute) -> T? {
+    
+    func getAttributeValue<T>(_ attribute: NSAccessibility.Attribute) -> T? {
         var value: AnyObject?
         
         let result = AXUIElementCopyAttributeValue(self, attribute.rawValue as CFString, &value)
         
         guard result == .success else {
-            print(result.rawValue)
             return nil
         }
-       
+        
         return value as? T
     }
     
@@ -26,18 +29,16 @@ extension AXUIElement {
         let result = AXUIElementPerformAction(self, action.rawValue as CFString)
         
         guard result == .success else {
-            print(result.rawValue)
             throw result
         }
     }
     
-    func getId() -> CGWindowID? {
+    func getWindowID() -> CGWindowID? {
         var windowID = CGWindowID(0)
         
         let result = _AXUIElementGetWindow(self, &windowID)
         
         guard result == .success else {
-            print(result.rawValue)
             return nil
         }
         
@@ -45,10 +46,9 @@ extension AXUIElement {
     }
 }
 
+// Make AXError conform to Error protocol
+extension AXError: @retroactive Error {}
+
+// Private Core Accessibility API
 @_silgen_name("_AXUIElementGetWindow") @discardableResult
 func _AXUIElementGetWindow(_ axUiElement: AXUIElement, _ wid: UnsafeMutablePointer<CGWindowID>) -> AXError
-
-@_silgen_name("GetProcessForPID") @discardableResult
-func GetProcessForPID(_ pid: pid_t, _ psn: UnsafeMutablePointer<ProcessSerialNumber>) -> OSStatus
-
-extension AXError: @retroactive Error {}
