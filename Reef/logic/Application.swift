14f280d213873739fbf09429980d43128a64eb78
Xander <gouws.xander@gmail.com>
2026-01-21 13:55:37 -0400

    Implement Application.getWindows(), deprecate .getCGWindowList

--- Reef/logic/Application.swift
+++ Reef/logic/Application.swift
@@ -128,27 +128,22 @@ class Application {
         let configuration = NSWorkspace.OpenConfiguration()
         NSWorkspace.shared.openApplication(at: bundleURL, configuration: configuration) { _, _ in }
     }
-    
-    func getCGWindowList() -> [[String: Any]] {
-        let options: CGWindowListOption = [.optionOnScreenOnly, .excludeDesktopElements]
-        guard let allWindows = CGWindowListCopyWindowInfo(options, kCGNullWindowID) as? [[String: Any]] else {
-            return []
-        }
-        
-        // TODO: Change to use PID? Maybe use both with ||
-        let applicationWindows = allWindows.filter { window in
-            return window["kCGWindowOwnerName"] as! String == self.title
-        }
-        
-        return applicationWindows
-    }
-    
+
+    // NOTE: Only returns windows in current Desktop (but multiple monitors does work)
     func getAXWindows() -> [AXUIElement] {
         guard let windows: [AXUIElement] = self.element.getAttributeValue(.windows) else {
             return []
         }
         
         return windows
+    }
+    
+    func getWindows() -> [Window] {
+        let axWindows = self.getAXWindows()
+        
+        return axWindows.map { axWindow in
+            Window(axWindow, self)
+        }
     }
     
     func listAvailableAttributes() -> [String] {
@@ -161,6 +156,21 @@ class Application {
         
         return attributes
     }
+    
+//    func getCGWindowList() -> [[String: Any]] {
+//        let options: CGWindowListOption = [.optionOnScreenOnly, .excludeDesktopElements]
+//        guard let allWindows = CGWindowListCopyWindowInfo(options, kCGNullWindowID) as? [[String: Any]] else {
+//            return []
+//        }
+//
+//        // TODO: Change to use PID? Maybe use both with ||
+//        let applicationWindows = allWindows.filter { window in
+//            return window["kCGWindowOwnerName"] as! String == self.title
+//        }
+//
+//        return applicationWindows
+//    }
+    
 }
 
 
